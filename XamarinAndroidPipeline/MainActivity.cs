9ba678c6e325a6ad7edd2738ac47ae6b63f2bb7b
Damien AICHEH <damien.aicheh@gmail.com>
2019-09-03 15:34:44 +0200

    Initial commit.

---
+++ XamarinAndroidPipeline/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace XamarinAndroidPipeline
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
+
